f0ec0f99548d13c0dea6b3f0bbb88b913830454e
Carl Thyness <carl.thyness@gmail.com>
2022-04-30 10:19:49 +0200

    Further reformating

--- Iris_project.m
+++ Iris_project.m
@@ -12,34 +12,9 @@ function MSE = Iris_project(Nfeatures, alpha)
     Ntrain = 30;
     bs = ones(Ntrain);
     bs = bs(1, :);
-    x1_train = zeros(Ntrain, dimx);
-    x1_train(: , 1:dimx) = x1(1:Ntrain, :);
-    x1_train(:, dimx+1) = bs;
+    [x_train, x_test] = splitSamples(Ntrain, Ntot, dimx, bs, x1, x2, x3);
     
-    x2_train = zeros(Ntrain, dimx);
-    x2_train(: , 1:dimx) = x2(1:Ntrain, :);
-    x2_train(:, dimx+1) = bs;
-    
-    x3_train = zeros(Ntrain, dimx);
-    x3_train(: , 1:dimx) = x3(1:Ntrain, :);
-    x3_train(:, dimx+1) = bs;
-    
-    x1_test = zeros(Ntot-Ntrain, dimx);
-    x1_test(: , 1:dimx) = x1(Ntrain+1:Ntot, :);
-    x1_test(:, dimx+1) = bs(1:20);
-    
-    x2_test = zeros(Ntot-Ntrain, dimx);
-    x2_test(: , 1:dimx) = x3(Ntrain+1:Ntot, :);
-    x2_test(:, dimx+1) = bs(1:20);
-    
-    x3_test = zeros(Ntot-Ntrain, dimx);
-    x3_test(: , 1:dimx) = x3(Ntrain+1:Ntot, :);
-    x3_test(:, dimx+1) = bs(1:20);
-    
-    % Puts all training and test sets in one matrix
-    x_train = [x1_train; x2_train; x3_train];
-    x_test = [x1_test; x2_test; x3_test];
-    
+    %Creates an array for checking results
     t_train = zeros(Nclasses, Ntrain*3);
     for i = 1:Nclasses
         t_train(i, Ntrain*(i-1)+1:Ntrain*i) = bs;
@@ -48,40 +23,13 @@ function MSE = Iris_project(Nfeatures, alpha)
     % Start training our model
     for i = 1:1000
         %Calculates the full g matrix
-        g = zeros(Nclasses, Ntrain);
-        for k = 1:Ntrain*3
-            for c = 1:Nclasses
-                w_class = W(c, :);
-                current_x = x_train(k, :);
-                g(c, k) = dot(w_class, current_x);
-                g(c, k) = 1/(1+exp(-g(c,k)));
-            end
-        end
+        g_train = calcg(Nclasses, Ntrain, x_train, W);
     
         %calculates current MSE and prints it
-        MSE = 0;
-        for k = 1:Ntrain*3
-            dMSE = dot(g(:, k)-t_train(:,k), g(:, k));
-            dMSE = dMSE*(bs(1:3)'-g(:, k));
-            dMSE = dot(dMSE, x_train(k, :)');
-    
-            MSE = MSE + dMSE;
-        end
-        disp(MSE);
+        MSE = calcMSE(Ntrain, x_train, g_train, t_train, bs);
     
         %Calculates current gradient of MSE dependent on W
-    
-        gMSE = zeros(Nclasses, dimx+1); % Gradient of MSE
-        for k = 1:Ntrain*3
-            dgMSE = g(:, k)-t_train(:, k); % delta gradient MSE
-            dgMSE = dot(dgMSE, g(:, k));
-            dgMSE = dgMSE * (bs(1:3)' - g(:, k));
-            temp = x_train(k, :);
-            dgMSE = dgMSE * temp;
-    
-            gMSE = gMSE + dgMSE;
-        end
-        disp(gMSE);
+        gMSE = calcGradientMSE(Nclasses, dimx, Ntrain, t_train, g_train, x_train, bs);
     
         break %Just testing my code so far, do not want it to run a thousand times yet
     end
@@ -126,9 +74,72 @@ function W = createW(Nclasses, dimx) %Creates a stupid W
     W(3, :) = [class3_feature_values(1:dimx) w0];
 end
 
+function [x_train, x_test] = splitSamples(Ntrain, Ntot, dimx, bs, x1, x2, x3)
+    x1_train = zeros(Ntrain, dimx);
+    x1_train(: , 1:dimx) = x1(1:Ntrain, :);
+    x1_train(:, dimx+1) = bs;
+    
+    x2_train = zeros(Ntrain, dimx);
+    x2_train(: , 1:dimx) = x2(1:Ntrain, :);
+    x2_train(:, dimx+1) = bs;
+    
+    x3_train = zeros(Ntrain, dimx);
+    x3_train(: , 1:dimx) = x3(1:Ntrain, :);
+    x3_train(:, dimx+1) = bs;
+    
+    x1_test = zeros(Ntot-Ntrain, dimx);
+    x1_test(: , 1:dimx) = x1(Ntrain+1:Ntot, :);
+    x1_test(:, dimx+1) = bs(1:20);
+    
+    x2_test = zeros(Ntot-Ntrain, dimx);
+    x2_test(: , 1:dimx) = x3(Ntrain+1:Ntot, :);
+    x2_test(:, dimx+1) = bs(1:20);
+    
+    x3_test = zeros(Ntot-Ntrain, dimx);
+    x3_test(: , 1:dimx) = x3(Ntrain+1:Ntot, :);
+    x3_test(:, dimx+1) = bs(1:20);
+    
+    % Puts all training and test sets in one matrix
+    x_train = [x1_train; x2_train; x3_train];
+    x_test = [x1_test; x2_test; x3_test];
+end
 
+function g = calcg(Nclasses, Ntrain, x, W)
+    g = zeros(Nclasses, Ntrain);
+        for k = 1:Ntrain*3
+            for c = 1:Nclasses
+                w_class = W(c, :);
+                current_x = x(k, :);
+                g(c, k) = dot(w_class, current_x);
+                g(c, k) = 1/(1+exp(-g(c,k)));
+            end
+        end
+end
 
+function MSE = calcMSE(N, x, g, t, bs)
+    MSE = 0;
+        for k = 1:N*3
+            dMSE = dot(g(:, k)-t(:,k), g(:, k));
+            dMSE = dMSE*(bs(1:3)'-g(:, k));
+            dMSE = dot(dMSE, x(k, :)');
+    
+            MSE = MSE + dMSE;
+        end
+        disp(MSE);
+end
 
+function gMSE = calcGradientMSE(Nclasses, dimx, N, t, g, x, bs) %Calculates the gradient of the MSE dependent on W
+    gMSE = zeros(Nclasses, dimx+1); 
+        for k = 1:N*3
+            dgMSE = g(:, k)-t(:, k); % delta gradient MSE
+            dgMSE = dot(dgMSE, g(:, k));
+            dgMSE = dgMSE * (bs(1:3)' - g(:, k));
+            dgMSE = dgMSE * x(k, :);
+    
+            gMSE = gMSE + dgMSE;
+        end
+        disp(gMSE);
+end
 
 
 
