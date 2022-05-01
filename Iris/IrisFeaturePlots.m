8b931555fe165b328f61bf6d90415bb1a37529f0
IvarStrangstadstuen <71649354+IvarStrangstadstuen@users.noreply.github.com>
2022-05-01 14:53:17 +0200

    Update IrisFeaturePlots.m

--- Iris/IrisFeaturePlots.m
+++ Iris/IrisFeaturePlots.m
@@ -7,24 +7,24 @@ ylabel('Sepal width');
 figure(2);
 gscatter(meas(:,1), meas(:,3), species,'rgb','osd');
 xlabel('Sepal length');
-ylabel('Pedal length');
+ylabel('Petal length');
 
 figure(3);
 gscatter(meas(:,1), meas(:,4), species,'rgb','osd');
 xlabel('Sepal length');
-ylabel('Pedal width');
+ylabel('Petal width');
 
 figure(4);
 gscatter(meas(:,2), meas(:,3), species,'rgb','osd');
 xlabel('Sepal width');
-ylabel('Pedal length');
+ylabel('Petal length');
 
 figure(5);
 gscatter(meas(:,2), meas(:,4), species,'rgb','osd');
 xlabel('Sepal width');
-ylabel('Pedal width');
+ylabel('Petal width');
 
 figure(6);
 gscatter(meas(:,3), meas(:,4), species,'rgb','osd');
-xlabel('Pedal length');
-ylabel('Pedal width');
+xlabel('Petal length');
+ylabel('Petal width');
